c0fb19e9411eaab8691cb7beb29a88e224228de2
yurijg@hotmail.com <yurijg@hotmail.com>
2025-01-15 06:09:24 +0000

    Add project files.

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCRUDApp.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Post[] postsToSeed = new Post[10];
+            for (int i = 1; i <= 10; i++)
+            {
+                postsToSeed[i - 1] = new Post
+                {
+                    Id = i,
+                    Title = $"Post {i}",
+                    Content = $"This is some example content for post {i}. The content is very interesting and I just wanted it to be two sentences long."
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
+    }
+}
